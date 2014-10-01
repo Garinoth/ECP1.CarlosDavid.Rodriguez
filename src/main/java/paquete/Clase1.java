fac39089068737fc390bfb188195066b43e135a9
CDavidRP <cdavidrp@gmail.com>
2014-10-01 13:03:49 +0200

    Añadir método resetarAtributo1

--- src/main/java/paquete/Clase1.java
+++ src/main/java/paquete/Clase1.java
@@ -1,8 +1,9 @@
 package paquete;
 
 public class Clase1 {
-    
+
     private int atributo1;
+
     private int atributo2;
 
     public Clase1(int atributo1, int atributo2) {
@@ -25,32 +26,36 @@ public class Clase1 {
     public void setAtributo1(int atributo1) {
         this.atributo1 = atributo1;
     }
-    
+
     public int restaAtributos() {
         return this.atributo1 - this.atributo2;
     }
-    
+
     public int multiplicacionAtributos() {
         return this.atributo1 * this.atributo2;
     }
 
-        public int sumaAtributos() {
+    public int sumaAtributos() {
         return this.atributo1 + this.atributo2;
     }
     
+    public void resetearAtributo1() {
+        this.atributo1 = 0;
+    }
+
     public void mostrarAtributos() {
         System.out.println("El atributo 1 es: " + this.atributo1);
         System.out.println("El atributo 2 es: " + this.atributo2);
     }
-    
+
     public void mostrarSuma() {
         System.out.println("La suma es: " + this.sumaAtributos());
     }
-    
+
     public void mostrarResta() {
         System.out.println("Resta: " + this.restaAtributos());
     }
-    
+
     public void mostrarMultiplicación() {
         System.out.println("Multiplicación: " + this.restaAtributos());
     }
