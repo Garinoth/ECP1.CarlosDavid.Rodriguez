05dfa3cef42f42a951b6f0ae86db754c826ce23b
CDavidRP <cdavidrp@gmail.com>
2014-10-01 09:34:13 +0200

    Añadir getters y setters para los atributos

--- src/main/java/paquete/Clase1.java
+++ src/main/java/paquete/Clase1.java
@@ -10,4 +10,19 @@ public class Clase1 {
         this.atributo2 = atributo2;
     }
 
+    public int getAtributo1() {
+        return this.atributo1;
+    }
+
+    public int getAtributo2() {
+        return this.atributo2;
+    }
+
+    public void setAtributo2(int atributo2) {
+        this.atributo2 = atributo2;
+    }
+
+    public void setAtributo1(int atributo1) {
+        this.atributo1 = atributo1;
+    }
 }
