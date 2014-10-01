4c3f0656675687714908f2a0df9bc7f99a0fc22c
CDavidRP <cdavidrp@gmail.com>
2014-10-01 09:31:39 +0200

    Añadir atributo2 a Clase1

--- src/main/java/paquete/Clase1.java
+++ src/main/java/paquete/Clase1.java
@@ -3,9 +3,11 @@ package paquete;
 public class Clase1 {
     
     private int atributo1;
+    private int atributo2;
 
-    public Clase1(int atributo1) {
+    public Clase1(int atributo1, int atributo2) {
         this.atributo1 = atributo1;
+        this.atributo2 = atributo2;
     }
 
 }
